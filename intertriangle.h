5d5d0602b186805c2202d29ef07289c8f411fa73
IshiiV <ishii.v.4@gmail.com>
2016-12-05 17:43:16 -0300

    Create intertriangle.h

---
+++ intertriangle.h
@@ -0,0 +1,23 @@
+#ifndef INTERTRIANGLE_H
+#define INTERTRIANGLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "crossproduct.h"
+#include "dot.h"
+#include "point3d.h"
+
+using namespace std;
+
+class Intertriangle
+{
+public:
+	 Intertriangle();
+     boolean intersecao(const Point3D V1, const Point3D V2, const Point3D V3, const Point3D O, const Point3D D);
+
+private:
+     Dot dot;
+     CrossProduct crossProduct;
+};
+
+#endif // INTERTRIANGLE_H
